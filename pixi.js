80f06f07fd4ef416f78a7d417a967cfadb4c85a7
Deepak Ram Sutrakar <deepakramsutrakar@gmail.com>
2024-03-21 12:47:38 +0530

    Create pixi.js

---
+++ pixi.js
@@ -0,0 +1,9 @@
+    // Tiling sprite - Start //
+    // const bgbackMountaintexture = PIXI.Texture.from(BgBackMountain);
+    // const bgbackMountain = new PIXI.TilingSprite(bgbackMountaintexture, app.screen.width, app.screen.height);
+    // app.ticker.add(()=>{
+    //     bgbackMountain.tilePosition.x -= 0.1;
+        
+    // })
+    // app.stage.addChild(bgbackMountain);
+    // Tiling sprite - End //
